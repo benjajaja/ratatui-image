326efa30d6aed3e6064ba13b235735710c35fce7
Benjamin Große <ste3ls@gmail.com>
2024-12-06 13:03:17 +0000

    don't send DECERA for sixels
    until we get sixel transparency anyway

--- src/protocol/sixel.rs
+++ src/protocol/sixel.rs
@@ -123,12 +123,13 @@ fn render(rect: Rect, data: &str, area: Rect, buf: &mut Buffer, overdraw: bool) 
         Some(r) => r,
     };
 
-    let top = render_area.top() + 1;
-    let left = render_area.left();
-    let bottom = render_area.bottom() + 1;
-    let right = render_area.right();
-    let seq = format!("\x1b[{top};{left};{bottom};{right}$z{data}");
-    buf.cell_mut(render_area).map(|cell| cell.set_symbol(&seq));
+    // DECERA
+    // let top = render_area.top() + 1;
+    // let left = render_area.left() + 1;
+    // let bottom = render_area.bottom() + 1;
+    // let right = render_area.right() + 1;
+    // let seq = format!("\x1b[{top};{left};{bottom};{right}$z{data}");
+    buf.cell_mut(render_area).map(|cell| cell.set_symbol(data));
     let mut skip_first = false;
 
     // Skip entire area
